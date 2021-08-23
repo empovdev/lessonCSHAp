2b66f340495936d6deeec0e373c9da0097a80f1e
EM POV <ahpov1113@gmail.com>
2021-08-23 19:59:28 +0700

    fix

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a; long b; double p1; string name;
+            Console.Clear();
+            Console.Write("Input Val a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input val b : ");
+            b = long.Parse(Console.ReadLine());
+            Console.Write("Input val P1 : ");
+            p1 = double.Parse(Console.ReadLine());
+            Console.Write("Input  Name : ");
+            name = Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
